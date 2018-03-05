3ddff7df41506a45c79974fcf5ed605cd68fe130
Daniel P <scorpionprogramming@gmail.com>
2018-03-05 11:27:59 +0100

    Datenbank eintrag kurs php hinzugefuegt
    - Datenbank für die Kurse erstellt
    - Kurs.php Datenbank connect hinzugefuegt. (TODO

--- www/kurs.php
+++ www/kurs.php
@@ -1,3 +1,62 @@
+<?php
+    $id = 1;
+    $record = null;
+
+    // Beispiel: 
+    // https://www.php-kurs.com/mysql-datenbank-auslesen.htm
+    // http://www.iti.fh-flensburg.de/lang/php/tabelle-darstellen.htm
+    // ^^^^^ alles was ich brauche
+
+
+    $db = new mysqli("localhost", "root", "", "kurs");
+    if($db->connect_errno){
+        printf("Connection failed: %s\n", $db->connect_error);
+        exit;
+    }
+
+    require_once ('konfiguration.php');
+    $db_link = mysqli_connect (
+                        MYSQL_HOST, 
+                        MYSQL_BENUTZER, 
+                        MYSQL_KENNWORT, 
+                        MYSQL_DATENBANK
+                        );
+    
+    $sql = "SELECT * FROM adressen";
+    
+    $db_erg = mysqli_query( $db_link, $sql );
+    if ( ! $db_erg )
+    {
+    die('Ungültige Abfrage: ' . mysqli_error());
+    }
+    
+    echo '<table border="1">';
+    while ($zeile = mysqli_fetch_array( $db_erg, MYSQL_ASSOC))
+    {
+    echo "<tr>";
+    echo "<td>". $zeile['id'] . "</td>";
+    echo "<td>". $zeile['nachname'] . "</td>";
+    echo "<td>". $zeile['vorname'] . "</td>";
+    echo "<td>". $zeile['akuerzel'] . "</td>";
+    echo "<td>". $zeile['strasse'] . "</td>";
+    echo "<td>". $zeile['plz'] . "</td>";
+    echo "<td>". $zeile['telefon'] . "</td>";
+    echo "</tr>";
+    }
+    echo "</table>";
+    
+    mysqli_free_result( $db_erg );
+
+    $record_alle_kurse = array();
+    $select = "SELECT * FROM kurse";
+    if($result = $db->query($select)){
+        while($kurs = $result->fetch_assoc()){
+            $record_alle_kurse[$kurs["id"]] == 
+        }
+        $result->free();
+    }
+?>
+
 <html>
 <head>
     <?php include("includes.html");?>
