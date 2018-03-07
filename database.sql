23d489aab851970b8c3212ceeab23fa2eeafd397
Unknown <soelterj98@gmx.de>
2018-03-07 12:47:02 +0100

    login?

--- database.sql
+++ database.sql
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS `lehrer` (
   `name` varchar(50) NOT NULL,
   `vorname` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `passwort` varchar(32) NOT NULL,
+  `passwort` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- www/login.php
+++ www/login.php
@@ -6,26 +6,44 @@
 		<link rel="stylesheet" type="text/css" href="../style/login.css">
     </head>
   <body class="text-center">
-    <form class="form-signin">
+    <form action="?login=1" method="post" class="form-signin">
       Planner<img class="mb-4" src="../images/logo.png" alt="logo">rama
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
       <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
       <label for="inputPassword" class="sr-only">Password</label>
       <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-      <div class="checkbox mb-3">
+      <!--<div class="checkbox mb-3">
         <label>
           <input type="checkbox" value="remember-me"> Remember me
         </label>
-      </div>
+      </div>-->
       <button class="btn btn-lg btn-dark btn-block" type="submit">Sign in</button>
       <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
     </form>
   </body>
 </html>
+<?php require_once("JSIncludes.html");?>
 
-
+<?php 
+session_start();
+ $pdo = connect();
+if(isset($_GET['login'])) {
+	echo "dsfvdfbgrf";
+    $email = $_POST['email'];
+    $passwort = $_POST['passwort'];
+    
+    $statement = $pdo->prepare("SELECT * FROM users WHERE email = :email");
+    $result = $statement->execute(array('email' => $email));
+    $user = $statement->fetch();
         
-        
-    </body>
-</html>
+    //Überprüfung des Passworts
+    if ($user !== false && password_verify($passwort, $user['passwort'])) {
+        $_SESSION['userid'] = $user['id'];
+        die('Login erfolgreich. Weiter zu <a href="geheim.php">internen Bereich</a>');
+    } else {
+        $errorMessage = "E-Mail oder Passwort war ungültig<br>";
+    }
+    
+}
+?>
