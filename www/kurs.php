c4cda71b624df38bdaebd67cddc99990411eacb5
Daniel P <scorpionprogramming@gmail.com>
2018-03-14 17:12:00 +0100

    Kurs - ID's auslesen udn Abspeichern
    - ID's werden nun in die Tabelle hinzugefügt
    - Sidebar Test
    - JQuerry wieder eingebunden ( wird für Sidebar gebraucht )

--- www/kurs.php
+++ www/kurs.php
@@ -6,6 +6,41 @@
     <?php include("header.html");?>
 
 
+<!-- Sidebar -->
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand">
+                <a href="#">
+                    Start Bootstrap
+                </a>
+            </li>
+            <li>
+                <a href="#">Dashboard</a>
+            </li>
+            <li>
+                <a href="#">Shortcuts</a>
+            </li>
+            <li>
+                <a href="#">Overview</a>
+            </li>
+            <li>
+                <a href="#">Events</a>
+            </li>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Services</a>
+            </li>
+            <li>
+                <a href="#">Contact</a>
+            </li>
+        </ul>
+    </div>
+    <!-- /#sidebar-wrapper -->
+    
+    <a href="#menu-toggle" class="btn btn-secondary" id="menu-toggle">Toggle Menu</a>
+    
     <?php
         // Verbindung zur Datenbank herstellen
         require_once "connectDB.php";
@@ -47,19 +82,28 @@
                 echo "<option>$row[1]</option>";
             }
         }
+    
+        function getFach($fach){
+            echo $fach;
+            $sql = "SELECT ID FROM planer.faecher WHERE bezeichnung = '$fach'";
+            $result = getQueryResult($sql) or exit("Fehler bei SQL Kommando: $sql");
+            $row = mysqli_fetch_row($result);
+            echo $row[0];
+            return $row[0];
+        }
 
         function saveKurs(){
             //kopiert aus "schueler.php"
             $connection = connect();
-            $id = 5;
+            $id = 9;
             if($connection != null){
                 //echo "hidden: ".$_POST['hiddenID'];
                 //$id = ($_POST['hiddenID'] < 0) ? getNextID() : $_POST['hiddenID'];
                 $id += 1;
                 $lehrerID = 1;
-                $kursID = 2;
+                $fachID = getFach($_POST['fach']);
                 $kursBez = $_POST['kursBez'];
-                $query = "INSERT INTO planer.kurs (id, bezeichnung, lehrerID, fachID) VALUES ('$id', '$kursBez', '$lehrerID', '$kursID');";
+                $query = "INSERT INTO planer.kurs (id, bezeichnung, lehrerID, fachID) VALUES ('$id', '$kursBez', '$lehrerID', '$fachID');";
                 echo $query;
                 
                 //nochmal schauen was hier gemacht werden muss
@@ -69,7 +113,7 @@
             }
         }
     ?>
-
+    
     <form action="#" method="POST">
         <div class="form-group">
             <label for="inputAddress">Kurs Bezeichnung</label>
@@ -88,9 +132,18 @@
         <div class="form-group row">
             <div class="col-sm-10">
                 <button type="submit" class="btn btn-primary" name="insert">Hinzufügen</button>
+                <button type="button" class="btn btn-danger" name="delete">Löschen</button>
             </div>
         </div>
     </form>
+    
+    <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
 </body>
 </html>
 
